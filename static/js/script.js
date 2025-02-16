7e7974cdaef259621e602d450c4849e57a64f074
Nicola <nicola.annese07@gmail.com>
2025-02-16 19:32:24 +0100

    avanzata

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,9 @@
-// script.js
-document.getElementById('loadJokeBtn').addEventListener('click', loadJoke);
+
 
 function loadJoke() {
-    const category = document.getElementById('category').value;
+    const category = document.querySelector("#category").value;
     let url = 'https://api.chucknorris.io/jokes/random';
 
-    // Se è selezionata una categoria diversa da 'none', aggiungi la categoria all'URL
     if (category !== 'none') {
         url = `https://api.chucknorris.io/jokes/random?category=${category}`;
     }
@@ -14,10 +12,13 @@ function loadJoke() {
         .then(response => response.json())
         .then(data => {
             // Mostra la barzelletta nel div 'jokeContainer'
-            document.getElementById('jokeContainer').textContent = data.value;
+            document.querySelector("#jokeContainer").innerHTML = data.value;
         })
         .catch(error => {
             console.error('Errore nel recupero della barzelletta:', error);
-            document.getElementById('jokeContainer').textContent = "Oops! C'è stato un errore.";
+            document.querySelector("#jokeContainer").innerHTML = "Oops! C'è stato un errore.";
         });
 }
+
+let btn = document.querySelector("#loadJokeBtn");
+btn.addEventListener("click", loadJoke);
